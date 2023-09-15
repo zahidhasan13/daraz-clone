19f339f0f0771a5b298496b6dc943280df29d08e
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-09-15 21:27:06 +0600

    footer complete

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,17 +1,27 @@
-import bangladesh from '../../assets/ban.png';
-import pakistan from '../../assets/pak.png';
-import srilanka from '../../assets/sl.png';
-import mayanmar from '../../assets/mnmr.png';
-import nepal from '../../assets/nep.png';
-import cash from '../../assets/cash.png'
+import appGallery from "../../assets/appgallery.png";
+import apple from "../../assets/apple.png";
+import bangladesh from "../../assets/ban.png";
+import bkash from "../../assets/bkash.png";
+import cash from "../../assets/cash.png";
+import google from "../../assets/google.png";
+import master from "../../assets/master.png";
+import mayanmar from "../../assets/mnmr.png";
+import nagad from "../../assets/nagad.png";
+import nepal from "../../assets/nep.png";
+import pakistan from "../../assets/pak.png";
+import qrCode from "../../assets/qr-code2.png";
+import rocket from "../../assets/rocket.jpg";
+import srilanka from "../../assets/sl.png";
+import visa from "../../assets/visa.png";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="bg-[#F5F5F5] py-4">
+      <div className="max-w-screen-xl mx-auto grid grid-cols-4">
       <div>
         <div>
-          <h3>Customer Care</h3>
-          <ul>
+          <h3 className="text-xl text-[#F85606] font-medium mb-2">Customer Care</h3>
+          <ul className="space-y-1">
             <li>Help Center</li>
             <li>How to Buy</li>
             <li>Returns & Refunds</li>
@@ -20,9 +30,9 @@ const Footer = () => {
             <li>CCMS - Central Complain Management System</li>
           </ul>
         </div>
-        <div>
-          <h3>Earn with Daraz</h3>
-          <ul>
+        <div className="mt-8">
+          <h3 className="text-xl text-[#F85606] font-medium mb-2">Earn with Daraz</h3>
+          <ul className="space-y-1">
             <li>Daraz University</li>
             <li>Sell on Daraz</li>
             <li>Code of Conduct</li>
@@ -31,8 +41,8 @@ const Footer = () => {
         </div>
       </div>
       <div>
-        <h3>Daraz</h3>
-        <ul>
+        <h3 className="text-xl text-[#F85606] font-medium mb-2">Daraz</h3>
+        <ul className="space-y-1">
           <li>About Daraz</li>
           <li>Digital Payments</li>
           <li>Daraz Blog</li>
@@ -48,40 +58,57 @@ const Footer = () => {
       </div>
       <div>
         <div>
-            <h3>Daraz International</h3>
-            <div>
-                <div>
-                    <img src={bangladesh} alt="" />
-                    <span>Bangladesh</span>
-                </div>
-                <div>
-                    <img src={pakistan} alt="" />
-                    <span>Bangladesh</span>
-                </div>
-                <div>
-                    <img src={srilanka} alt="" />
-                    <span>Bangladesh</span>
-                </div>
-                <div>
-                    <img src={mayanmar} alt="" />
-                    <span>Bangladesh</span>
-                </div>
-                <div>
-                    <img src={nepal} alt="" />
-                    <span>Bangladesh</span>
-                </div>
+          <h3 className="text-xl text-[#F85606] font-medium mb-2">Daraz International</h3>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex items-center">
+              <img src={bangladesh} alt="" className="w-8 mr-2"/>
+              <span>Bangladesh</span>
             </div>
+            <div className="flex items-center">
+              <img src={pakistan} alt="" className="w-8 mr-2"/>
+              <span>Bangladesh</span>
+            </div>
+            <div className="flex items-center">
+              <img src={srilanka} alt="" className="w-8 mr-2"/>
+              <span>Bangladesh</span>
+            </div>
+            <div className="flex items-center">
+              <img src={mayanmar} alt="" className="w-8 mr-2"/>
+              <span>Bangladesh</span>
+            </div>
+            <div className="flex items-center">
+              <img src={nepal} alt="" className="w-8 mr-2"/>
+              <span>Bangladesh</span>
+            </div>
+          </div>
         </div>
-        <div>
-            <h3>Payment Methods</h3>
-            <img src={cash} alt="" />
+        <div className="mt-8">
+          <h3 className="text-xl text-[#F85606] font-medium mb-2">Payment Methods</h3>
+          <div className="flex flex-wrap space-x-2 space-y-2 items-center">
+          <img src={cash} alt="" className="w-20 h-16 bg-slate-200 p-2 rounded"/>
+          <img src={visa} alt="" className="w-20 h-16 bg-slate-200 p-2 rounded"/>
+          <img src={master} alt="" className="w-20 h-16 bg-slate-200 p-2 rounded"/>
+          <img src={bkash} alt="" className="w-20 h-16 bg-slate-200 p-2 rounded"/>
+          <img src={nagad} alt="" className="w-20 h-16 bg-slate-200 p-2 rounded"/>
+          <img src={rocket} alt=""className="w-20 h-16 bg-slate-200 p-2 rounded" />
+          </div>
         </div>
-        <div></div>
       </div>
       <div>
-        <div></div>
+        <div>
+          <h3 className="text-xl text-[#F85606] font-medium mb-2">Exclusive Deals and Offers!</h3>
+          <div className="flex items-center">
+            <img src={qrCode} alt="" className="w-32"/>
+            <div className="space-y-2 ml-4">
+              <img src={apple} alt="" className="w-24"/>
+              <img src={google} alt="" className="w-24"/>
+              <img src={appGallery} alt="" className="w-24"/>
+            </div>
+          </div>
+        </div>
         <div></div>
       </div>
+    </div>
     </div>
   );
 };
